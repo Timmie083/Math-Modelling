0b28d77da3ff1fb4d8922b10120ec8d9c5885852
Timmie083 <tli083@uit.no>
2026-05-01 11:41:14 +0200

    Assig 5.2 done

--- assignment_template/sat_lib.py
+++ assignment_template/sat_lib.py
@@ -6,6 +6,10 @@ import simulator as sim
 import plotter as pl
 import math
 
+###################################
+# Assignment 4 | Classes          #
+###################################
+
 class RigidBody(sim.BaseScenario):
     def __init__(self):
         
@@ -13,11 +17,17 @@ class RigidBody(sim.BaseScenario):
         self.omega = None   #Angular velocity [3]
         self.tau = None     #Torque [3]
         self.J = None       #Inertia matrix [3x3]
+
+        self.r = None       #Position
+        self.v = None       #Velocity
+        self.F = None       #Force
+        self.m = None       #Mass
+
         self.RK4_x = None
 
-
     def init(self, t):
-
+        
+        # Rotation
         self.q = np.array([1.0, 0, 0, 0])
         self.omega = np.array([0, 0, 5])
         self.tau = np.array([0, 0, 0])
@@ -27,7 +37,14 @@ class RigidBody(sim.BaseScenario):
             [0,     0.1,    2.5]
         ])
 
-        self.RK4_x = np.hstack((self.q, self.omega))
+        # Translation
+        self.r = np.array([7000, 0, 0])   # [km]
+        self.v = np.array([0, 7.5, 0])      # [km/s]
+        self.F = np.array([0, 0, 0])      # Force [N]
+        self.m = 1000.0                         # [kg]
+
+        # Full state vector
+        self.RK4_x = np.hstack((self.q, self.omega, self.r, self.v))
 
     def update(self, t, dt):
 
@@ -36,17 +53,27 @@ class RigidBody(sim.BaseScenario):
 
         self.RK4_x = su.step_RK4(dt, t, self.RK4_x, f_wrapper, ae=None)
 
+        # Unpack
         self.q = self.RK4_x[0:4]
         self.omega = self.RK4_x[4:7]
+        self.r = self.RK4_x[7:10]
+        self.v = self.RK4_x[10:13]
 
+        # Normalize quaternion
         self.q = self.q / np.linalg.norm(self.q)
 
     def f(self, t, x):
 
         q = x[0:4]
         omega = x[4:7]
+        r = x[7:10]
+        v = x[10:13]
 
-        # ----- Quaternion kinematics -----
+        # -----------------------
+        # ROTATION
+        # -----------------------
+
+        # Quaternion kinematics
         wx, wy, wz = omega
 
         Omega = np.array([
@@ -58,18 +85,31 @@ class RigidBody(sim.BaseScenario):
 
         q_dot = 0.5 * Omega @ q
 
-        # ----- Rigid body dynamics -----
-        J = self.J
-        tau = self.tau
+        # Rigid body dynamics
 
-        omega_dot = np.linalg.solve(J, tau - np.cross(omega, J @ omega))
+        omega_dot = np.linalg.solve(self.J, self.tau - np.cross(omega, self.J @ omega))
 
-        return np.hstack((q_dot, omega_dot))
+        # -----------------------
+        # TRANSLATION (NEW)
+        # -----------------------
+
+        r_dot = v
+
+        r_norm = np.linalg.norm(r)
+
+        a_gravity = -ol.mu * r / r_norm**3
+
+        # External force contribution
+        a_force = self.F / self.m
+
+        v_dot = a_gravity + a_force
+
+        return np.hstack((q_dot, omega_dot, r_dot, v_dot))
     
     def get(self):
         quat = su.Quaternion(self.q)
         return [
-            ['satellite', np.array([0, 0, 0]), quat],
+            ['satellite', self.r, quat],
     ]
 
 class Satellite(sim.BaseScenario):
@@ -136,3 +176,44 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+###################################
+# Assignment 5 | Classes          #
+###################################
+
+class ADCS_PD:
+
+    def __init__(self, k1, k2, f_c, J):
+
+        self.k1 = k1
+        self.k2 = k2
+        self.J = J
+        self.tau = np.zeros(3)
+
+    def update(self, q_ib, w_b_ib, q_io, w_i_io):
+
+        # Quaternion error (q_error = q_id^{-1} ⊗ q_ib)
+
+        q_e = su.quat_mult(su.quat_conj(q_io), q_ib)
+
+        if q_e[0] < 0:
+            q_e = -q_e
+
+        q_v = q_e[1:4]
+
+        # Angular velocity error
+
+            # Rotation matrix from error quaternion
+        R_e = su.quaternion_to_dcm(q_e)
+
+            # Convert desired angular velocity into body frame
+        w_d_body = R_e @ w_i_io
+
+        w_err = w_b_ib - w_d_body
+
+        # PD control law
+
+        self.tau = -self.k1 * q_v - self.k2 * w_err
+
+    def get_control(self):
+        return self.tau
